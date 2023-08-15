bc48dc418b8c9ab3099bb78065aa6fb06aff4653
BrainSedative <60501673+BrainSedative@users.noreply.github.com>
2023-08-15 19:40:38 +0300

    Revert "Тестовые изменения в ветке develop"
    This reverts commit a04e6d0d9e7260f7539394d8a1837bc35a52880d.

--- Form1.cs
+++ Form1.cs
@@ -29,18 +29,44 @@ namespace Pozdra
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-
+            BdayDatabaseEntities db = new BdayDatabaseEntities();
+            bdayperson.FullName = txtName.Text;
+            bdayperson.Position = txtPosition.Text;
+            bdayperson.DateOfBirth = dateTimePicker1.Value;
+            bdayperson.DaysUntilBirthday = dateTimePicker1.Value.DayOfYear - DateTime.Now.DayOfYear;
+            if (bdayperson.DaysUntilBirthday + dateTimePicker1.Value.DayOfYear < DateTime.Now.DayOfYear)
+                bdayperson.DaysUntilBirthday = dateTimePicker1.Value.DayOfYear + 365 - DateTime.Now.DayOfYear;
+            db.BdayPeoples.Add(bdayperson);
+            db.SaveChanges();
+            MessageBox.Show("Сохранение успешно");
+            PopulateDataGridView();
         }
 
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            BdayDatabaseEntities db = new BdayDatabaseEntities();
+            var entry = db.Entry(bdayperson);
+            if (entry.State == System.Data.Entity.EntityState.Detached)
+                db.BdayPeoples.Attach(bdayperson);
+            db.BdayPeoples.Remove(bdayperson);
+            db.SaveChanges();
+            PopulateDataGridView();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            BdayDatabaseEntities db = new BdayDatabaseEntities();
+            bdayperson.FullName = txtName.Text;
+            bdayperson.Position = txtPosition.Text;
+            bdayperson.DateOfBirth = dateTimePicker1.Value;
+            bdayperson.DaysUntilBirthday = dateTimePicker1.Value.DayOfYear - DateTime.Now.DayOfYear;
+            if (bdayperson.DaysUntilBirthday + dateTimePicker1.Value.DayOfYear < DateTime.Now.DayOfYear)
+                bdayperson.DaysUntilBirthday = dateTimePicker1.Value.DayOfYear + 365 - DateTime.Now.DayOfYear;
+            db.BdayPeoples.Add(bdayperson);
+            db.Entry(bdayperson).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            PopulateDataGridView();
         }
 
         private void gridBirthdays_DoubleClick(object sender, EventArgs e)
@@ -50,7 +76,14 @@ namespace Pozdra
 
         private void gridBirthdays_Click(object sender, EventArgs e)
         {
-
+            if (gridBirthdays.CurrentRow.Index != -1)
+            {
+                BdayDatabaseEntities db = new BdayDatabaseEntities();
+                bdayperson.Id = (int)gridBirthdays.CurrentRow.Cells["Id"].Value;
+                bdayperson = db.BdayPeoples.Where(x => x.Id == bdayperson.Id).FirstOrDefault();
+                txtName.Text = bdayperson.FullName;
+                txtPosition.Text = bdayperson.Position;
+            }
         }
 
 
